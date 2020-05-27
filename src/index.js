9f5e2cfed1fee771cdea4551d4677364d99f2bdd
chenjianke <1317671142@qq.com>
2020-05-27 21:22:25 +0800

    loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 // commonJs
 // const Header = require('./header.js');
 // ES Module
-import Header from './header.js';
-// console.log(Header);
-new Header();
+// import Header from './header.js';
+import image from './img.jpg';
+// console.log(image);
+let ele = document.getElementById('root');
+let img = new Image();
+img.src = image;
+ele.append(img);
+// new Header();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js', // 打包的文件
+  module: {
+    rules: [{
+      test: /\.jpg$/,
+      use: {
+        loader: 'file-loader'
+      }
+    }]
+  },
   output: {
     filename: 'bundle.js', // 打包后的文件名称
     path: path.resolve(__dirname, 'dist') // 打包后的文件路径 __dirname webpack.config.js所在的当前路径 bundle 文件夹名称

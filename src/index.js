22a3370262d06d1b083fe19bdc9dc10a51412b26
chenjianke <1317671142@qq.com>
2020-10-14 23:18:14 +0800

    plugins

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@
 // ES Module
 // import Header from './header.js';
 import image from './img.jpg';
+import './index.scss'
 // console.log(image);
 let ele = document.getElementById('root');
 let img = new Image();
+img.classList.add("avatar")
 img.src = image;
 ele.append(img);
 // new Header();
+console.log(1232);
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,80 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js', // 打包的文件
   module: {
     rules: [{
-      test: /\.jpg$/,
-      use: {
-        loader: 'file-loader'
-      }
-    }]
+      test: /\.(jpg|png|gif)$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            // placeholder 占位符
+            name: '[name]_[hash].[ext]',
+            outputPath: 'images/'
+          }
+        },
+        {
+          loader: 'file-loader',
+          // options: {
+          //   name: '[path][name].[ext]'
+          // }
+        }
+        
+      ]
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: { importLoaders: 2 },
+        },
+        'sass-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [
+                [
+                  'autoprefixer',
+                  {
+                    // Options
+                  },
+                ],
+              ],
+            },
+          },
+        },
+          // options: {
+          //   postcssOptions: {
+          //     plugins: [
+          //       [
+          //         'autoprefixer',
+          //         {
+          //           // Options
+          //         },
+          //       ],
+          //     ],
+          //   },
+          // }
+        // }
+      ] // css-loader 合并css style-loader 挂载 style 至html head部分 sass-loader 处理sass成css
+    }
+  ]
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'webpack-study',
+      template: 'src/index.html'
+    })
+  ],
   output: {
-    filename: 'bundle.js', // 打包后的文件名称
+    filename: 'dist.js', // 打包后的文件名称
     path: path.resolve(__dirname, 'dist') // 打包后的文件路径 __dirname webpack.config.js所在的当前路径 bundle 文件夹名称
   }
 }
